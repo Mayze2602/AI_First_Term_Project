79d3feba2d12e425ff9c03c4b63fde3b9b85c8cd
Mayze2602 <juanestebanmayoral@gmail.com>
2024-02-25 18:37:00 -0600

    cleaned repo

--- normalizeGraph.py
+++
@@ -1,8 +0,0 @@
-import networkx as nx
-def normalize(graph_og):
-    graph = nx.DiGraph()
-    for node in graph_og.nodes():
-        graph.add_node(node.lower())
-    for edge in graph_og.edges(data=True):
-        graph.add_edge(edge[0].lower(), edge[1].lower(), weight=edge[2]['weight'])
-    return graph
